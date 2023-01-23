fb9016addd532f1d024d5d30ac7df3a3816cf8c7
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-01-23 18:40:47 +0530

    asd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 2917;
+const port =  process.env.PORT||2917;
 const cors = require("cors");
 const cookiesParser = require("cookie-parser");
 app.use(express.json());
