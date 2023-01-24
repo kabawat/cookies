78de899a2165d15aebc320e0a89d2ef21298829a
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-01-24 14:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 });
 // app.use(cookiesParser());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://cookietrytwo.netlify.app");
+  res.header("Access-Control-Allow-Origin", "https://cookietrytwot.netlify.app");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "content-type, withCredentials, Content-Type, withcredentials");
   next();
