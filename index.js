7b1f78e15fb796e6288e6fb4c06088c9242112e8
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-01-24 11:29:11 +0530

    update

--- index.js
+++ index.js
@@ -14,11 +14,9 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Headers", "withCredentials");
-  res.header("Access-Control-Allow-Headers", "content-type");
-    res.header("Access-Control-Allow-Headers", "withcredentials");
-  
   next();
 });
+
 app.post("/login", (req, res) => {
   // res.header["Access-Control-Allow-Origin"] = "https://cookiestry.netlify.app";
   console.log(req.body);
