1f5ffa0ef1aba10a73d0eee2ef617e653b935921
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-01-24 11:11:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 2917;
+const port = process.env.PORT || 2917;
 const cors = require("cors");
-const cookiesParser = require("cookie-parser");
+// const cookiesParser = require("cookie-parser");
 app.use(express.json());
-app.use(cookiesParser());
-app.use(
-  cors({
-    origin: "https://cookietry.netlify.app",
-    credentials: true,
-  })
-);
 app.use(express.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
-
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  res.cookie("hi", "hello", {
-    sameSite: "none",
-    secure: true,
-  });
-  res.set('Access-Control-Allow-Origin', 'http://cookiestry.netlify.com');
-  res.send("done");
+// app.use(cookiesParser());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://cookiestry.netlify.app");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
-
-app.get("/cookies", (req, res) => {
-  console.log(req.cookies);
-  res.json(req.cookies);
+app.post("/login", (req, res) => {
+  // res.header["Access-Control-Allow-Origin"] = "https://cookiestry.netlify.app";
+  console.log(req.body);
+  res.setHeader("Set-Cookie", "auth=token SameSite=None; Secure");
+  res.send(req.body);
 });
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`http://192.168.0.5:${port}`);
 });
